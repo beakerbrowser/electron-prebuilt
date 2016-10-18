884300675c1b03ffa35d8ef15d8146f2cbb2a594
Paul Frazee <pfrazee@gmail.com>
2016-10-18 14:14:04 -0500

    switch mirror to beakerbrowser/electron

--- install.js
+++ install.js
@@ -41,7 +41,8 @@ download({
   platform: process.env.npm_config_platform,
   arch: process.env.npm_config_arch,
   strictSSL: process.env.npm_config_strict_ssl === 'true',
-  quiet: process.env.npm_config_loglevel === 'silent'
+  quiet: process.env.npm_config_loglevel === 'silent',
+  mirror: 'https://github.com/beakerbrowser/electron/releases/download/v'
 }, extractFile)
 
 // unzips and makes path.txt point at the correct executable
